06ac83bb68c372fb6728b30421b0efdd7444d69d
Alsdnworks <alsdnworks@gmail.com>
2022-11-11 16:13:31 +0900

    SQL외부코드처리

--- main/views.py
+++ main/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 import json
 from django.core.serializers import serialize
 from .models import JoinedLink
@@ -9,13 +8,6 @@ class MarkersMapView(TemplateView):
     template_name = "map.html"
     def get_context_data(self, **kwargs):
         """Return the view context data."""
-        from django.db import connection
-        cursor=connection.cursor()
-        ##########부등호를 바꿔서 6시간 이상인 것들을 삭제하고, 6시간 이하인 것들을 가져온다.----오류!!
-        cursor.execute("DELETE from public.main_marker where (now() - date) > interval '6 hour';")
-        cursor.execute("TRUNCATE main_joinedlink;")
-        cursor.execute("INSERT INTO main_joinedlink overriding system value select nextval('main_joinedlink_id_seq'), main_link.location, main_marker.stat from main_link,main_marker  where ST_CONTAINS(main_link.location, main_marker.location) ;")
-        connection.close() 
         context = super().get_context_data(**kwargs)
         context["markers"] = json.loads(serialize("geojson", JoinedLink.objects.all()))
         return context
